1dc4790f38c1da5742ce3bf82d06907cf926a980
Ong Zi Jian <156037046+Ong415@users.noreply.github.com>
2025-08-29 23:25:38 +0800

    Delete src directory

--- src/adt/interfaces/List.java
+++
@@ -1,9 +0,0 @@
-package adt.interfaces;
-
-public interface List<T> extends Collection<T> {
-    T get(int index);
-    T set(int index, T value);
-    default void add(T element) { insert(size(), element); }
-    boolean insert(int index, T element);
-    T remove(int index);
-}
